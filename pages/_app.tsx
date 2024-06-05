49320f84009d4cae0e9b17311d82652b1755d5e6
mac <mac@macui-MacBookPro.local>
2024-06-05 18:36:30 +0900

    feat: meta tags integration on document private file

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,5 +2,8 @@ import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
+  console.log("APP COMPONENT - PAGES ROUTER");
+
   return <Component {...pageProps} />;
 }
+// _  2ta fayllarni ichida bir qator vazifalarni hal qilish un suniy maydon yasab beradi. bular o'zini bir qator tayanch vazifalariga ega
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,10 +1,29 @@
 import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
-  console.log("DOCUMENT - PAGES ROUTER");
+  // console.log("DOCUMENT - PAGE ROUTER");
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <meta charSet="UTF-8" />
+        <meta name="title" content="Nestar Demo" />
+        <meta name="robots" content="index, follow" />  {/* search engine un */}
+        <link rel="icon" type="image/png" href="/img/logo/favicon.svg" />
+
+        {/** SEO -> Search Enginge Optimization degani **/}
+        <meta
+          name="keyword"
+          content={"nestar, nestar.uz, devex mern, mern nestjs fullstack"}
+        />
+        <meta
+          name="description"
+          content={
+            "Buy and sell properties anywhere anytime in South Korea. |  " +
+            "Покупайте и продавайте недвижимость в любой точке Южной Кореи в любое время." +
+            "대한민국 언제 어디서나 부동산 사고팔 수 있습니다."
+          }
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
@@ -12,3 +31,8 @@ export default function Document() {
     </Html>
   );
 }
+
+/** private  birinci kirganda to'liq server site rendering usulidi quriladi
+ * searching injing optimizationni hosil qilish un yordamga keladi. va loyihaga tegshli metadetalrni integratsiyasini hosil qilamz
+ metadetalar  biror linkga kirmasdan turib uni malumotlarini olish imkonini beradi m. telegram
+*/
