d4b711b097f6f5e3145cc948c0ed4888898be28a
mac <mac@macui-MacBookPro.local>
2024-06-05 19:03:03 +0900

    feat: MUI customized integration

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,20 @@
-import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import { CssBaseline } from "@mui/material";
+import { useState } from "react";
+import { light } from "../scss/MaterialTheme";
 
 export default function App({ Component, pageProps }: AppProps) {
-  console.log("APP COMPONENT - PAGES ROUTER");
+  //@ts-ignore
+  const [theme, setTheme] = useState(createTheme(light));
 
-  return <Component {...pageProps} />;
+  // Socket.io, Redux, Mui....
+  return (
+    <ThemeProvider theme={theme}>      {/* mui parent qilsak page routing tizimni hamma faylda integratsiya qilingan bo'ladi. props sifatida pass bo'lyapti */}
+      <CssBaseline />
+\
+      <Component {...pageProps} />      {/* app o'zidagi narsalarni hammasini qolgan pages routingda joylashgan fayllarga jo'natyapti */}
+    </ThemeProvider>
+  );
 }
 // _  2ta fayllarni ichida bir qator vazifalarni hal qilish un suniy maydon yasab beradi. bular o'zini bir qator tayanch vazifalariga ega
