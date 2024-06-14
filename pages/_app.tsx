a139212f8e043cdb23665a2ff70a47dee84aa244
mac <mac@macui-MacBookPro.local>
2024-06-14 16:49:53 +0900

    feat: develop apollo client connection

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,23 +3,26 @@ import { ThemeProvider, createTheme } from "@mui/material/styles";
 import { CssBaseline } from "@mui/material";
 import { useState } from "react";
 import { light } from "../scss/MaterialTheme";
+import { ApolloProvider } from "@apollo/client";
+import client from "@/apollo/client";
 import "../scss/app.scss";
 import "../scss/pc/main.scss"; // scss larni chaqirdik
 import "../scss/mobile/main.scss";
-
 
 // app global integratsiyasi uchun kerak. har bir faylda app ishga tushadi
 export default function App({ Component, pageProps }: AppProps) {
   //@ts-ignore
   const [theme, setTheme] = useState(createTheme(light));
 
-  // Socket.io, Redux, Mui....
+  // Socket.io, Redux, Mui, Apollo Client ....
   return (
-    <ThemeProvider theme={theme}>      {/* mui parent qilsak page routing tizimni hamma faylda integratsiya qilingan bo'ladi. props sifatida pass bo'lyapti */}
-      <CssBaseline />
-
-      <Component {...pageProps} />      {/* app o'zidagi narsalarni hammasini qolgan pages routingda joylashgan fayllarga jo'natyapti */}
-    </ThemeProvider>
+    <ApolloProvider client={client}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </ApolloProvider>
   );
 }
 // _  2ta fayllarni ichida bir qator vazifalarni hal qilish un suniy maydon yasab beradi. bular o'zini bir qator tayanch vazifalariga ega
+ /* app o'zidagi narsalarni hammasini qolgan pages routingda joylashgan fayllarga jo'natyapti */
